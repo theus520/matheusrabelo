a58623cf68d169ba79b81ff15913dda4741ec67c
Laboratorio <laboratorio@uniprojecao.edu.br>
2019-08-29 20:08:02 -0300

    crei um site

---
+++ Untitled3.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main (){
+	
+ int x;
+ 
+  printf("digite o valor numerico\n");
+  scanf("%d",&x);
+  
+//	printf("x sera maior ou igual 5");
+	
+	if (x>=5)
+	{
+		printf("x eh maior\n");
+		
+	}else{
+		
+	
+	printf("x e menor \n");
+	
+	}
+	
+	
+	
+	return 0;
+}
